01f486b1a094201302e2727c7e114f4184025ae4
asifb <asifb@coventry.ac.uk>
2023-01-28 08:33:44 +0000

    Test image has been added. Code can now open and process this image.

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ encoded = input("Encoded Message: ")
 print(f"\nImage = {filename} \nSecret message = {encoded}")
 
 
-if filename.endswith("png"):
-    print("correct")
+def open_image(filename):
+    if filename.endswith("png"):
+        opnImg = cv2.imread(filename)
+        cv2.imshow('Your image: ', opnImg)
+        cv2.waitKey(0)
 
-else:
-    print("\nPlease add an image with png suffix")
+    else:
+        print("\nPlease add an image with the png suffix")
 
+
+open_image(filename)
+
+#cv2.imwrite('dog.jpg', opnImg)
