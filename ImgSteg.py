c4fdce412c6594c5a23a18a137f100a5a97ef419
asifb <asifb@coventry.ac.uk>
2023-01-29 03:50:29 +0000

    - Due to successful testing on test1.py, this file's contents now switches with ImgSteg.py (Main file)

--- ImgSteg.py
+++ ImgSteg.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 
 
 def open_image():  # This function is used to load the inputted image using the OpenCV module
@@ -9,29 +8,33 @@ def open_image():  # This function is used to load the inputted image using the 
             opnImg = cv2.imread(fileName)
             cv2.imshow('Your inputted image: ', opnImg)
             cv2.waitKey(0)
-            break
+            return opnImg
+
         else:
             print("\nPlease choose a valid image file!!")
 
 
-open_image()
 
 
 def message_to_binary():  # Before encoding the image, we need to first convert the code to binary (https://www.thepythoncode.com/article/hide-secret-data-in-images-using-steganography-python)
     encodeMessage = input("Encoded Message: ")
-    #if type(encodeMessage) == str or type(encodeMessage) == int:
-    if isinstance(encodeMessage,str):
+    # if type(encodeMessage) == str or type(encodeMessage) == int:
+    if encodeMessage == "":
+        print("Please include a message to encode")
+    else:
         enBin = ''.join([format(ord(i), "08b") for i in encodeMessage])
         print(f"\nYour message has been converted into binary: {enBin}")
-    else:
-        print(TypeError("Program can only convert text to binary"))
+        return enBin
 
 
-message_to_binary()
 
-def encode(En):
-    enBin = message_to_binary()
-
+def encode():
+    opnImg=open_image()
+    enBin=message_to_binary()
     print(f"the test is {enBin}")
+    cv2.imshow('TEST: ', opnImg)
+    cv2.waitKey(0)
+
+
 
 encode()
--- test1.py
+++ test1.py
@@ -2,40 +2,36 @@ import cv2
 import numpy as np
 
 
-def open_image():  # This function is used to load the inputted image using the OpenCV module
+
+def message_to_binary():  # Before encoding the image, we need to first convert the code to binary (https://www.thepythoncode.com/article/hide-secret-data-in-images-using-steganography-python)
+    while True:
+        encodeMessage = input("Encoded Message: ")
+        print(type(encodeMessage))
+        if encodeMessage == "":
+            print("\nPlease include a message")
+
+        else:
+            enBin = ''.join([format(ord(i), "08b") for i in encodeMessage])
+            print(f"\nYour message has been converted into binary: {enBin}")
+            break
+
+# type(encodeMessage) == str:
+
+
+def encode_image():
     while True:
         fileName = input("\nImage Filename with extension: ")
         if fileName.endswith("png") or fileName.endswith("jpg") or fileName.endswith("jpeg"):  # This checks to see if the image is valid
             opnImg = cv2.imread(fileName)
             cv2.imshow('Your inputted image: ', opnImg)
             cv2.waitKey(0)
-            return opnImg
-
+            noBytes =  opnImg.shape[0] * opnImg.shape[1] * 3 // 8
+            print("[*] Maximum bytes to encode:", noBytes)
+            break
         else:
             print("\nPlease choose a valid image file!!")
 
 
+encode_image()
+message_to_binary()
 
-
-def message_to_binary():  # Before encoding the image, we need to first convert the code to binary (https://www.thepythoncode.com/article/hide-secret-data-in-images-using-steganography-python)
-    encodeMessage = input("Encoded Message: ")
-    # if type(encodeMessage) == str or type(encodeMessage) == int:
-    if encodeMessage == "":
-        print("Please include a message to encode")
-    else:
-        enBin = ''.join([format(ord(i), "08b") for i in encodeMessage])
-        print(f"\nYour message has been converted into binary: {enBin}")
-        return enBin
-
-
-
-def encode():
-    opnImg=open_image()
-    enBin=message_to_binary()
-    print(f"the test is {enBin}")
-    cv2.imshow('TEST: ', opnImg)
-    cv2.waitKey(0)
-
-
-
-encode()
