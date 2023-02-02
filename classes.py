676a676c64eeb03df89311898fb18535b9e6fec8
asifb <asifb@coventry.ac.uk>
2023-02-02 00:14:56 +0000

    - Encoding function attempted. However, is not currently functioning correctly. OpenCV implementation needed

--- classes.py
+++ classes.py
@@ -19,39 +19,43 @@ class ImgSteg:
             print("\nERROR: Please choose a valid image.")
             quit()
 
-
+    def message_check(self):
+        if self.encodeMessage == "":
+            print("ERROR: Please include a message to encode")
+            quit()
 
     def encode(self):
-        height, width, channels = self.img.shape
-        pixelArray = np.array(self.img)
-
-        if channels == 3:
-            n = 3
-        elif channels == 4:
-            n = 4
-
-        totalPixels = pixelArray //self.n
-        self.encodeMessage += "DDDDD" # Delimiter
+        height, width = self.img.shape[0], self.img.shape[1]
+        pixelArray = self.img.ravel()
+        print(pixelArray)
+        totalPixels = pixelArray.size // 3
+        self.encodeMessage += "DDDDD"  # Delimiter
         binaryMessage = ''.join([format(ord(i), "08b") for i in self.encodeMessage])
         requiredPixels = len(binaryMessage)
 
-        if (requiredPixels > totalPixels).all():
+        if requiredPixels > (totalPixels * 3):
             print("ERROR, please reduce your message or choose a bigger image ")
             quit()
 
         else:
-            check_index=0
-            for pixel in range(totalPixels):
-                for q in range(0,3):
+            check_index = 0
+            for p in range(totalPixels):
+                for q in range(0, 3):
                     if check_index < requiredPixels:
-                        pixelArray[pixel][q] = int(bin(pixelArray[p][q]) [2:9] + binaryMessage[pixel], 2)
-                        check_index +=1
-            pixelArray = pixelArray.reshape(height,width)
-            cr
+                        pixelArray[p][q] = int(bin(pixelArray[p][q])[2:9] + binaryMessage[check_index], 2)
+                        check_index += 1
 
+            array = pixelArray.reshape(height, width, 3)
+            encodedImage = cv2.cvtColor(array, cv2.COLOR_RGB2BGR)
+            dest = input("Where do you wish to store the image")
+            cv2.imwrite(dest, encodedImage)
+            # encodedImage = cv2.cvtColour (array, cv2.COLOR_RGB2BGR)
+            # cv2.imwrite(f"{self.fileName}+_encoded.png", encodedImage)
+            print("Your message has been successfully encoded")
 
 
 if __name__ == '__main__':
     obj = ImgSteg()
     obj.open_image()
+    obj.message_check()
     obj.encode()
