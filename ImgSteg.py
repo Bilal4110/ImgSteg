d59b6cfccbcaad316f9a0b98fbcd982d6d858d83
asifb <asifb@coventry.ac.uk>
2023-02-04 23:08:28 +0000

    Start has been made on encoding function for ImgSteg.py.
      + function to encode message to image has been developed (References, optimisation and cleanup may be needed)
      + Binary converter has been moved into class
    
      TODO:
           + Create function/possible menu to implement and use the newly created encoder function
           + Find a way of adding _encoded prefix to created encoded images
           + Create and implement decoder function to show the encoded data to user
           + Binary converter may need redevelopment to ensure efficiency and reduce redundancy

--- ImgSteg.py
+++ ImgSteg.py
@@ -2,43 +2,69 @@ import cv2
 import numpy as np
 
 
-def binary_converter(
-        content):  # This function will be used to convert image pixels and the encoded message into their binary equivalent
-    if type(content) == str:
-        return ''.join([format(ord(i), "08b") for i in content])
 
-    elif type(content) == bytes or type(content) == np.ndarray:
-        return [format(i, "08b") for i in content]
 
-    elif type(content) == int or type(content) == np.uint8:
-        return format(content, "08b")
-    else:
-        print("input not supported, please try again")
+
+
 
 
 class ImgSteg:
-    fileName = input("\nImage Filename with extension (Please choose PNG images to ensure efficiency): ")
-    img = cv2.imread(fileName)
-    encodeMessage = input("Encoded Message:")
 
-    def encoder(self, img, hiddenMessage):
-        maximumBytes = img.size // 8
-        print(f"You have a maximum of {maximumBytes} bytes to encode...")
+    # This function will be used to convert image pixels and the encoded message into their binary equivalent
+    def binary_converter(self, content):
 
-        # The below check ensures that the image has enough space to encode the user-supplied message
 
-        if len(hiddenMessage) < maximumBytes:
-            print(
-                "ERROR: Insufficient space, please increase the size of the image or reduce the message you are trying to encode")
+        if type(content) == str:
+            return ''.join([format(ord(i), "08b") for i in content])
+
+        elif type(content) == int or type(content) == np.uint8:
+            return format(content, "08b")
+
+        elif type(content) == bytes or type(content) == np.ndarray:
+            return [format(i, "08b") for i in content]
 
         else:
-            # This is the delimiter which will be added to the message. (Any string can be used as a delimiter)
-            hiddenMessage += "====="
-            binaryMessage = binary_converter(hiddenMessage)
-            dataLength = len(binaryMessage)
-            index = 0
+            print("input not supported, please try again")
 
-            for values in img:
-                for pixel in values:
-                    r, g, b = binary_converter(pixel)
-                    if dataLength < index:
+    # This function takes in code
+    def encoder(self, img, hiddenMessage):
+
+        # Calculates size of the image
+        maximumBytes = img.size // 8
+
+        # The below check ensures that the image has enough space to encode the user-supplied message
+        if len(hiddenMessage) > maximumBytes:
+            print("\nERROR: Insufficient space, please increase the size of the image or reduce the message you are trying to encode!!!")
+
+        # This is the delimiter which will be added to the message. (Any string can be used as a delimiter)
+        hiddenMessage += "====="
+        binaryMessage = self.binary_converter(hiddenMessage)
+        dataLength = len(binaryMessage)
+        index = 0
+        for values in img:
+            for pixel in values:
+                # splits pixels into r,g,b and then converts them to their binary equivalent (ADD REFERENCE)
+                r, g, b = self.binary_converter(pixel)
+                if index < dataLength:
+                    pixel[0] = int(r[:-1] + binaryMessage[index], 2)
+                    index += 1
+                if index < dataLength:
+                    pixel[1] = int(g[:-1] + binaryMessage[index], 2)
+                    index += 1
+                if index < dataLength:
+                    pixel[2] = int(b[:-1] + binaryMessage[index], 2)
+                    index += 1
+
+                if index >= dataLength:
+                    break
+
+        return img
+
+
+
+
+
+if __name__ == "__main__":
+    obj = ImgSteg()
+
+
