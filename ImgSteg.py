b927eafb663700550a95d3c0b3434baa9d53ef79
asifb <asifb@coventry.ac.uk>
2023-02-04 15:03:28 +0000

    Start has been made on encoding function for ImgSteg.py.
      + function to calculate image size has been implemented
      + Check made to ensure encode message does not pass the total size of image
      + Delimiter and binary converter added to encoded message
      - Binary converter test variables and values removed from code.
    
      TODO:
           + Algorithm needs completion to successfully encode message inside image

--- ImgSteg.py
+++ ImgSteg.py
@@ -2,7 +2,8 @@ import cv2
 import numpy as np
 
 
-def binary_converter(content): # This function will be used to convert image pixels and the encoded message into their binary equivalent
+def binary_converter(
+        content):  # This function will be used to convert image pixels and the encoded message into their binary equivalent
     if type(content) == str:
         return ''.join([format(ord(i), "08b") for i in content])
 
@@ -12,7 +13,7 @@ def binary_converter(content): # This function will be used to convert image pix
     elif type(content) == int or type(content) == np.uint8:
         return format(content, "08b")
     else:
-        print ("input not supported, please try again")
+        print("input not supported, please try again")
 
 
 class ImgSteg:
@@ -20,15 +21,24 @@ class ImgSteg:
     img = cv2.imread(fileName)
     encodeMessage = input("Encoded Message:")
 
-    value = 255
-    convertedInt = binary_converter(value)
-    print(convertedInt)
-    value2 = "HELLO"
-    convertedstr = binary_converter(value2)
-    print(convertedstr)
+    def encoder(self, img, hiddenMessage):
+        maximumBytes = img.size // 8
+        print(f"You have a maximum of {maximumBytes} bytes to encode...")
 
+        # The below check ensures that the image has enough space to encode the user-supplied message
 
+        if len(hiddenMessage) < maximumBytes:
+            print(
+                "ERROR: Insufficient space, please increase the size of the image or reduce the message you are trying to encode")
 
+        else:
+            # This is the delimiter which will be added to the message. (Any string can be used as a delimiter)
+            hiddenMessage += "====="
+            binaryMessage = binary_converter(hiddenMessage)
+            dataLength = len(binaryMessage)
+            index = 0
 
-
-
+            for values in img:
+                for pixel in values:
+                    r, g, b = binary_converter(pixel)
+                    if dataLength < index:
