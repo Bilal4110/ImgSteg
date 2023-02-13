5e345916b0854990a5ebf3c07d0e82f6516c2c89
asifb <asifb@coventry.ac.uk>
2023-02-13 18:11:17 +0000

    + More detailed added to docstrings
    TODO:
    + Complete docstrings  for hide_text (encoding) function
    + hide_text function needs to be finalised and tested
    + Begin development of a decoding function to show the text to user

--- ImgSteg.py
+++ ImgSteg.py
@@ -1,11 +1,8 @@
-import cv2
+import cv2 # OpenCV-Module
 import numpy as np
 
 
 class ImgSteg:
-
-
-
 
     def open_image(self, img):
         """
@@ -25,27 +22,29 @@ class ImgSteg:
         return image
 
     def binary_converter(self, data):
-        """Converts a string (Binary message) or a numpy array (pixel) into its equivalent binary value
+        """
+        Converts a string (Binary message) or a numpy array (pixel) into its equivalent binary value
 
             :param data: Input data which needs to be converted into binary
             :type data: str or numpy.ndarray
             :return: The binary equivalent of the supplied data
-            :rtype: string or list of strings in binary equivalent
+            :rtype: string or list of strings in binary equivalent (Depending on the type of data inputted)
         """
 
         if type(data) == str:
             return ''.join([format(ord(i), "08b") for i in data])  # https://www.geeksforgeeks.org/python-convert-string-to-binary/
-        elif type(data) == np.ndarray:
+        elif type(data) == np.ndarray: # this is the data in the opened image
             return [format(i, "08b") for i in data]  # https://docs.replit.com/tutorials/python/steganography
 
     def hide_text(self, image, hiddenMessage):
-        """This function hides a text-based message into an image using the LSB method
+        """This function hides a text-based message into an image using the LSB method. It first calculates the sizes of both the image and the message (with delimiter) which needs to be encoded.
+            This is to ensure that there enough available bytes to encode. If there is enough space, then a message is encoded within the least significant bits of the image's pixels
 
             :param image: Image which will be used to store the encoded message (Cover Image)
             :param hiddenMessage: Message that will be encoded and hidden into the image. This is to be supplied by the user in the form of an input
             :type image: numpy.ndarray
             :type hiddenMessage: str
-            :return: The modified image data containing the hidden message
+            :returns: The modified image data containing the hidden message encoded within its pixels
             :rtype: numpy.ndarray
 
             References:https://medium.com/towards-data-science/hiding-data-in-an-image-image-steganography-using-python-e491b68b1372 - EXPAND!!!
@@ -77,7 +76,8 @@ class ImgSteg:
             return image
 
 
-    def encoder(self):
+    def encode(self):
+
         fileName = input("choose an image: ")
         if fileName.endswith(".png"):
             img = self.open_image(fileName)
@@ -87,8 +87,12 @@ class ImgSteg:
             if len(encodeMessage) == 0:
                 print("\nERROR: Please add a message to encode!")
 
+    def show_text(self, enImg):
+
+        pass
+
 
 
 if __name__ == "__main__":
     obj = ImgSteg()
-    obj.encoder()
+    obj.encode()
